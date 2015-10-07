617655e13800790e726a94667663be4b56af8aa6
SpencerWilliams <williamssp@nycap.rr.com>
2015-10-07 09:13:26 -0400

    Boilerplate Added
    GPL 2.0 Boilerplate added to only current source file. In the future, it
    will be added as they are created.

--- JukeUI.py
+++ JukeUI.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python3
 
-"""This script contains the GUI builder for the jukebox program. It is powered
-by GTK for simplicity. It contains classes for all the screens that might be
-required by the main script."""
+  """This script contains the GUI builder for the jukebox program. It is powered
+  by GTK for simplicity. It contains classes for all the screens that might be
+  required by the main script.
+  Copyright (C) {2015}  {Spencer R Williams}
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA."""
 
 from gi.repository import Gtk
 
